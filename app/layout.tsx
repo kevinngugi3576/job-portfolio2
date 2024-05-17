ef02efbf3eb841a872799d8acf7ab103f6f34c74
kevinngugi3576 <kevinngugi197@gmail.com>
2024-05-17 14:17:12 +0300

    changed case sensitive background for build process

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import StarsCanvas from "./components/starBackGround";
+import StarsCanvas from "./components/StarBackGround";
 import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
