fbd6570de89df4902a2d513472e45cfd283d0aa3
kevinngugi3576 <kevinngugi197@gmail.com>
2024-05-17 13:55:02 +0300

    Build the project and update code

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Skills from "@/app/components/Skills";
 import Encryption from "@/app/components/Encryption";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+
 const page = () => {
   return (
     <main className="h-full w-full">
