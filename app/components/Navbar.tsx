4391561ce866253076d1b3f7790a4406ee37956b
kevinngugi3576 <kevinngugi197@gmail.com>
2024-06-04 21:43:36 +0300

    added mobile responsiveness

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,26 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import NavLogo from "@/public/assets/NavLogo.png";
 import { Socials } from "@/constants/Index";
+import Hamburger from "hamburger-react";
 
 const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  const toggleNav = () => {
+    setOpen(!isOpen);
+  };
+
+  const handleClick = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="w-full h-[95px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px=-[10px]">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <div className="flex flex-row items-center">
           <Image
             src={NavLogo}
@@ -18,28 +29,36 @@ const Navbar = () => {
             height={70}
             className="cursor-pointer hover:animate-slowspin"
           />
-          <span className="font-bold ml-[10px] hidden md:block text-cyan-500 text-2xl">
+          <span className="font-bold ml-[10px] md:block text-cyan-500 text-2xl">
             The Webchain Development
           </span>
         </div>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div
-            className="flex items-center justify-between 
-            mr-[15px] px-[10px] py-[10px] rounded-lg text-white text-2xl w-full"
+        <div className="hidden lg:flex flex-row items-center justify-between md:mr-20 space-x-4">
+          <Link
+            href="#about me"
+            className="cursor-pointer hover:-translate-y-3 delay-150 font-bold text-white text-2xl"
+            onClick={handleClick}
           >
-            <Link href="#about me" className="cursor-pointer">
-              About Me
-            </Link>
-            <Link href="#skills" className="cursor-pointer">
-              skills
-            </Link>{" "}
-            <Link href="#projects" className="cursor-pointer">
-              Projects
-            </Link>
-          </div>
+            About Me
+          </Link>
+          <Link
+            href="#skills"
+            className="cursor-pointer hover:-translate-y-3 delay-150 font-bold text-white text-2xl"
+            onClick={handleClick}
+          >
+            Skills
+          </Link>
+          <Link
+            href="#projects"
+            className="cursor-pointer hover:-translate-y-3 delay-150 font-bold text-white text-2xl"
+            onClick={handleClick}
+          >
+            Projects
+          </Link>
         </div>
-        <div className="flex flex-wrap gap-5">
+
+        <div className="hidden lg:flex space-x-3">
           {Socials.map((social: any) => (
             <Link
               key={social.name}
@@ -56,7 +75,59 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
+
+        <div className="lg:hidden">
+          <button onClick={toggleNav} className="bg-purple-600">
+            <Hamburger toggled={isOpen} toggle={setOpen} />
+          </button>
+        </div>
       </div>
+
+      {isOpen && (
+        <div className=" inset-0 bg-[#030014] bg-opacity-95  flex flex-col items-center justify-center lg:hidden h-screen">
+          <Link
+            href="#about me"
+            className="cursor-pointer hover:-translate-y-3 delay-150 font-bold text-3xl text-white mb-4"
+            onClick={handleClick}
+          >
+            About Me
+          </Link>
+          <Link
+            href="#skills"
+            className="cursor-pointer hover:-translate-y-3 delay-150 font-bold text-3xl text-white mb-4"
+            onClick={handleClick}
+          >
+            Skills
+          </Link>
+          <Link
+            href="#projects"
+            className="cursor-pointer hover:-translate-y-3 delay-150 font-bold text-3xl text-white mb-4"
+            onClick={handleClick}
+          >
+            Projects
+          </Link>
+          <div className="flex space-x-4 mt-8">
+            {Socials.map((social: any) => (
+              <Link
+                key={social.name}
+                href={
+                  social.name === "gmail"
+                    ? "mailto:kevinngugi197@gmail.com"
+                    : ""
+                }
+                onClick={handleClick}
+              >
+                <Image
+                  src={social.src}
+                  alt={social.name}
+                  width={36}
+                  height={36}
+                />
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -3,11 +3,11 @@ import ProjectsCard from "./sub/ProjectsCard";
 
 const Projects = () => {
   return (
-    <div  id="projects" className=" flex flex-col items-center justify-center gap-8  py-20">
+    <div  id="projects" className=" flex flex-col items-center justify-center gap-8  py-20 ">
       <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My projects
       </h1>
-      <div className=" flex flex-row flex-nowrap items-center  gap-8 px-20 py-20 md:flex-row  ">
+      <div className=" flex flex-row flex-nowrap items-center  gap-8 px-20 py-20 md:flex-row sm:flex-col sm:size:64 ">
         <ProjectsCard
           src="/assets/portfolioPic.png"
           title="Modern Three.js Portfolio"
